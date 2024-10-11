b51884eaeb1ce4fb720f252a1e098550e61fa73c
Darius Mason <lyublanovits19@gmail.com>
2024-10-10 20:59:36 -0400

    Add project files.

---
+++ Project 1.cpp
@@ -0,0 +1,22 @@
+// Project 1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+using namespace std;
+
+double calculateCommission(double sales) {
+    if (sales < 10000) {
+        return sales * 0.05;
+    }else if (sales < 1500) {
+        return sales * 0.10;
+    }else if (sales < 18000) {
+        return sales * 0.12;
+    }else if (sales < 22000) {
+        return sales * 0.14;
+    }else
+}
+{
+    // 
+}
+
